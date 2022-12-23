ceb0e422b65de1b35ce616a8aa64c641352efc55
Barhamou <hama.peinture.barhamou@gmail.com>
2022-12-23 22:05:08 +0100

    5. The wonders of spread syntax tentative 2

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,1 @@
-export default function concatArrays(array1, array2, string) {
-  return array1.concat(array2.concat(string.split('')));
-}
+export default function concatArrays(array1, array2, string) { return array1.concat(array2.concat(string.split(''))); }

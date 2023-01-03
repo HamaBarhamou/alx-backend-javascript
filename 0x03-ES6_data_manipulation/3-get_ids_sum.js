210240a1fdfdf5980c1d72ce372926b5c0e91a9d
Barhamou <hama.peinture.barhamou@gmail.com>
2023-01-03 12:12:28 +0100

    3. Reduce tentative 2

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,4 @@
-import getListStudentIds from './1-get_list_student_ids.js';
-
 export default function getStudentIdsSum(tab) {
-  const idTab = getListStudentIds(tab);
   const initialValue = 0;
-  return idTab.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+  return tab.reduce((accumulator, currentValue) => accumulator + currentValue.id, initialValue);
 }

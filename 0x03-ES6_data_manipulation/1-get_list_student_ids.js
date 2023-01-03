20b617dcd5e0a1fc085a10c111adda6dbc2a6704
Barhamou <hama.peinture.barhamou@gmail.com>
2023-01-03 10:16:58 +0100

    eslint js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,12 @@
 export default function getListStudentIds(tab) {
   if (Array.isArray(tab)) {
     const idTab = new Array(tab.length);
-    for (let i = 0; i < tab.length; i += 1) {
-      idTab[i] = tab[i].id;
-    }
+    let i = 0;
+    tab.map((x) => {
+      idTab[i] = x.id;
+      i += 1;
+      return idTab[i];
+    });
     return idTab;
   }
   return [];
